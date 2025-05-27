55be6a550fa354ed04bb1c4793f46d41ccbc2710
A-manH <amanhuss3n@gmail.com>
2025-05-27 16:27:28 -0400

    init

---
+++ sqlite_test.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect("sqlite.db")
+cursor = connection.cursor()
+
+# cursor.execute('''ALTER TABLE fruits ADD COLUMN color VARCHAR(20)''')
+
+fruits = [
+    {"name": "apple", "color": "red"},
+    {"name": "banana", "color": "yellow"},
+    {"name": "grape", "color": "purple"},
+    {"name": "orange", "color": "orange"},
+    {"name": "kiwi", "color": "green"},
+    {"name": "blueberry", "color": "blue"},
+    {"name": "watermelon", "color": "green"},
+    {"name": "mango", "color": "orange"},
+    {"name": "lemon", "color": "yellow"},
+    {"name": "cherry", "color": "red"}, 
+    {"name": "pineapple", "color": "brown"},
+]
+
+# # for fruit in fruits:
+#     cursor.execute(f'''INSERT INTO fruits(name, color) VALUES(?, ?)''', (fruit["name"], fruit["color"]))
+#     connection.commit()
+
+cursor.execute('''SELECT * FROM fruits;''')
+results = cursor.fetchmany())
+print(results)
