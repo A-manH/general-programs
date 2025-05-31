4a6ab85d2fff243672dc35bee63f35ded402647d
A-manH <amanhuss3n@gmail.com>
2025-05-31 16:52:37 -0400

    learned grouping, special characters, exct

--- regular_expression.py
+++ regular_expression.py
@@ -3,7 +3,7 @@ import re
 # email = input()
 email = "name@test.gmail.com"
 match = re.search(rf"^(\w+)@(\w+\.)?gmail\.com$", email, re.IGNORECASE)
-
+test = 1
 print(match.group(2))
 
     # if expression:
